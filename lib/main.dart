376ac9fde23e7feb4948f10c8761de018ceaf6f7
hamed-fs <hamed@firstsource.io>
2020-02-02 16:40:56 +0330

    - add divider to suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: HomePage(title: 'Flutter Demo Home Page'),
     );
@@ -34,77 +34,87 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: TypeAheadField<String>(
-          getImmediateSuggestions: true,
-          textFieldConfiguration: TextFieldConfiguration(
-            controller: _textEditController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              filled: false,
-              hintText: 'What are you looking for?',
-              suffixIcon: InkWell(
-                onTap: _clearSearch,
-                borderRadius: BorderRadius.circular(25.0),
-                child: Icon(Icons.clear),
-              ),
-            ),
-          ),
-          hideOnEmpty: true,
-          suggestionsCallback: (String pattern) async {
-            return pattern.isEmpty
-                ? null
-                : ['Search*$pattern*in Store', 'Search*$pattern*in News'];
-          },
-          itemBuilder: (context, String suggestion) {
-            List<String> suggestionParts = suggestion.split('*');
-
-            return ListTile(
-              leading: Icon(
-                Icons.search,
-                size: 24.0,
-              ),
-              title: RichText(
-                text: TextSpan(
-                  text: '${suggestionParts[0]} ',
-                  style: TextStyle(color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '${suggestionParts[1]}',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextSpan(text: ' ${suggestionParts[2]}'),
-                  ],
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: TypeAheadField<String>(
+              getImmediateSuggestions: true,
+              textFieldConfiguration: TextFieldConfiguration(
+                controller: _textEditController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'What are you looking for?',
+                  suffixIcon: InkWell(
+                    onTap: _clearSearch,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Icon(Icons.clear),
+                  ),
                 ),
               ),
-            );
-          },
-          transitionBuilder: (
-            BuildContext context,
-            Widget suggestionsBox,
-            AnimationController animationController,
-          ) =>
-              FadeTransition(
-            child: suggestionsBox,
-            opacity: CurvedAnimation(
-              parent: animationController,
-              curve: Curves.easeOut,
-            ),
-          ),
-          suggestionsBoxDecoration: SuggestionsBoxDecoration(
-            elevation: 2.0,
-            borderRadius: BorderRadius.circular(4.0),
-          ),
-          onSuggestionSelected: (String suggestion) {
-            print("$suggestion selected");
+              hideOnEmpty: true,
+              suggestionsCallback: (String pattern) async {
+                return pattern.isEmpty
+                    ? null
+                    : ['Search*"$pattern"*in Store', 'Search*"$pattern"*in News'];
+              },
+              itemBuilder: (context, String suggestion) {
+                List<String> suggestionParts = suggestion.split('*');
 
-            _clearSearch();
-          },
-        ),
+                return Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(
+                        Icons.search,
+                        size: 24.0,
+                      ),
+                      title: RichText(
+                        text: TextSpan(
+                          text: '${suggestionParts[0]} ',
+                          style: TextStyle(color: Colors.black),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: '${suggestionParts[1]}',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(text: ' ${suggestionParts[2]}'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(height: 1.0)
+                  ],
+                );
+              },
+              transitionBuilder: (
+                BuildContext context,
+                Widget suggestionsBox,
+                AnimationController animationController,
+              ) =>
+                  FadeTransition(
+                child: suggestionsBox,
+                opacity: CurvedAnimation(
+                  parent: animationController,
+                  curve: Curves.easeOut,
+                ),
+              ),
+              suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                elevation: 2.0,
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              onSuggestionSelected: (String suggestion) {
+                print("$suggestion selected");
+
+                _clearSearch();
+              },
+            ),
+          )
+        ],
       ),
     );
   }
